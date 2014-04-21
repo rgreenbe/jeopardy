994bcb6b2dba5377331cb50f5b3b0f0ccc75ef03
Matt <mattgode@gmail.com>
2014-04-20 23:21:02 -0400

    Added some backend

--- src/github.com/cmu440/backend/backend_stub.go
+++ src/github.com/cmu440/backend/backend_stub.go
@@ -1,13 +1,23 @@
 package backend
 
 import (
+	"net"
 	"strings"
 )
 
 type stub struct {
 }
 
+func NewStub() Backend {
+	return &stub{}
+}
+
 func (s *stub) RevcCommit(commitMessage []byte) error {
-	message := string(commitMessage)
-	items := strings.Split(message, ",")
+	commit := string(commitMessage)
+	items := strings.Split(commit, ",")
+	hostPort := items[0]
+	message := items[1]
+	conn, err := net.Dial("tcp", hostPort)
+	conn.Write([]byte(message))
+	conn.Close()
 }
